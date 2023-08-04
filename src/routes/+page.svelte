d3353efab54068bb1a6fe89f04d107d67e39761f
Ajay Sharma <hi@ajaysharma.dev>
2023-08-05 01:56:22 +0530

    partially working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,10 @@
 <!-- App.svelte -->
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import uuid from 'uuid';
+	import { onDestroy, onMount } from 'svelte';
+	import { v4 } from 'uuid';
 	const signalingServerUrl = 'ws://localhost:8080';
-	let id = uuid.v4();
-	let ws: WebSocket;
-
-	let video1: HTMLVideoElement, stream: MediaStream, video2: HTMLVideoElement;
-
-	let localConnection: any;
-	let remoteConnection: any;
-	let sendChannel: any;
-	let receiveChannel: any;
-	let dataChannelSendValue = '';
-	let dataChannelReceiveValue = '';
-	let isSendChannelOpen = false;
+	let id = v4();
+	let ws: WebSocket | null = null;
 
 	onMount(() => {
 		ws = new WebSocket(signalingServerUrl);
@@ -22,46 +12,62 @@
 
 		// Handle incoming messages from the signaling server
 		ws.onmessage = (event) => {
+			if (!connection) return;
 			const message = JSON.parse(event.data);
 
 			if (message.type === 'offer') {
 				gotOffer(message);
 			} else if (message.type === 'answer') {
-				gotDescription2(message);
+				gotAnswer(message);
 			} else if (message.type === 'iceCandidate') {
-				onIceCandidate(localConnection!, message);
+				onIceCandidate(message);
 			}
 		};
+
+		const servers = null;
+		connection = new RTCPeerConnection(servers);
 	});
 
-	const createConnection = () => {
-		const servers = null;
-		localConnection = localConnection = new RTCPeerConnection(servers);
-		console.log('Created local peer connection object localConnection');
+	onDestroy(() => {
+		ws = null;
+		connection = null;
+	});
 
-		sendChannel = localConnection.createDataChannel('sendDataChannel');
+	let video1: HTMLVideoElement, stream: MediaStream, video2: HTMLVideoElement;
+
+	let connection: any;
+	let sendChannel: any;
+	let receiveChannel: any;
+	let dataChannelSendValue = '';
+	let dataChannelReceiveValue = '';
+	let isSendChannelOpen = false;
+
+	const createConnection = () => {
+		console.log(connection);
+		console.log('Created local peer connection object connection');
+
+		sendChannel = connection.createDataChannel('sendDataChannel');
 		console.log('Created send data channel');
 
-		localConnection.onicecandidate = (e) => {
-			onIceCandidate(localConnection, e);
+		connection.onicecandidate = (event) => {
+			console.log(event, 'ice');
+			if (event.candidate) {
+				sendMessage({ id, candidate: event.candidate, type: 'iceCandidate' });
+			}
 		};
-		sendChannel.onopen = onSendChannelStateChange;
-		sendChannel.onclose = onSendChannelStateChange;
 
 		if (stream) {
 			stream.getTracks().forEach((track) => {
-				localConnection!.addTrack(track, stream!);
+				console.log(track.id);
+				connection!.addTrack(track, stream!);
 			});
 		}
+		sendChannel.onopen = onSendChannelStateChange;
+		sendChannel.onclose = onSendChannelStateChange;
 
-		remoteConnection = remoteConnection = new RTCPeerConnection(servers);
-		console.log('Created remote peer connection object remoteConnection');
-
-		remoteConnection.onicecandidate = (e) => {
-			onIceCandidate(remoteConnection, e);
-		};
-		remoteConnection.ondatachannel = receiveChannelCallback;
-		remoteConnection.ontrack = (event) => {
+		connection.ondatachannel = receiveChannelCallback;
+		connection.ontrack = (event) => {
+			console.log(event);
 			// Event contains the received MediaStreamTrack
 			// You can attach it to the video element as shown below
 			if (video2.srcObject !== event.streams[0]) {
@@ -69,9 +75,10 @@
 			}
 		};
 
-		localConnection
-			.createOffer()
-			.then((desc) => sendMessage({ id, desc, type: 'offer' }), onCreateSessionDescriptionError);
+		connection.createOffer().then((desc) => {
+			connection.setLocalDescription(desc);
+			sendMessage({ id, desc, type: 'offer' });
+		}, onCreateSessionDescriptionError);
 	};
 
 	// Function to send data to the signaling server
@@ -93,44 +100,39 @@
 		console.log('Closed data channel with label: ' + sendChannel.label);
 		receiveChannel.close();
 		console.log('Closed data channel with label: ' + receiveChannel.label);
-		localConnection.close();
-		remoteConnection.close();
-		localConnection = null;
-		remoteConnection = null;
+		connection.close();
+		connection = null;
 		console.log('Closed peer connections');
 		dataChannelSendValue = '';
 		dataChannelReceiveValue = '';
 	};
 
-	const gotDescription1 = (desc) => {
-		localConnection.setLocalDescription(desc);
-		console.log(`Offer from localConnection\n${desc.sdp}`);
-		sendMessage({ type: 'offer', desc: desc });
-		remoteConnection.setRemoteDescription(desc);
-		remoteConnection.createAnswer().then(gotDescription2, onCreateSessionDescriptionError);
+	const gotOffer = (message) => {
+		if (message?.id !== id) {
+			connection.setRemoteDescription(message.desc);
+			console.log(`Offer from connection\n${message.desc}`);
+			connection.createAnswer().then((desc) => {
+				connection.setLocalDescription(desc);
+				sendMessage({ id, desc, type: 'answer' });
+			}, onCreateSessionDescriptionError);
+		}
 	};
 
-	const gotDescription2 = (desc) => {
-		remoteConnection.setLocalDescription(desc);
-		console.log(`Answer from remoteConnection\n${desc.sdp}`);
-		localConnection.setRemoteDescription(desc);
+	const gotAnswer = (message) => {
+		if (message?.id !== id) {
+			connection.setRemoteDescription(message?.desc);
+			console.log(`Answer from remoteConnection\n${message?.desc.sdp}`);
+		}
 	};
 
-	const getOtherPc = (pc) => {
-		return pc === localConnection ? remoteConnection : localConnection;
-	};
-
-	const getName = (pc) => {
-		return pc === localConnection ? 'localPeerConnection' : 'remotePeerConnection';
-	};
-
-	const onIceCandidate = (pc, event) => {
-		getOtherPc(pc)
-			.addIceCandidate(event.candidate)
-			.then(onAddIceCandidateSuccess, onAddIceCandidateError);
-		console.log(
-			`${getName(pc)} ICE candidate: ${event.candidate ? event.candidate.candidate : '(null)'}`
-		);
+	const onIceCandidate = (message) => {
+		if (message?.id !== id && message?.candidate) {
+			connection
+				.addIceCandidate(message.candidate)
+				.then(onAddIceCandidateSuccess)
+				.catch(onAddIceCandidateError);
+			console.log(`ICE candidate received and added.`, 'id', message.id, message.candidate);
+		}
 	};
 
 	function onAddIceCandidateSuccess() {
@@ -175,14 +177,18 @@
 			// Get the MediaStream from the video element
 			//@ts-ignore
 			stream = video1.captureStream();
-			// video2.srcObject = stream;
-			// You can use the 'stream' variable as needed
+			if (stream) {
+				console.log(stream);
+				stream.getTracks().forEach((track) => {
+					connection!.addTrack(track, stream!);
+				});
+			}
 		}
 	};
 </script>
 
 <main>
-	<button on:click={createConnection} disabled={isSendChannelOpen}>Start</button>
+	<button on:click={createConnection} disabled={false}>Start</button>
 	<textarea id="dataChannelSend" bind:value={dataChannelSendValue} disabled={!isSendChannelOpen} />
 	<button on:click={sendData} disabled={!isSendChannelOpen}>Send</button>
 	<textarea
@@ -195,7 +201,7 @@
 
 <video id="user1" bind:this={video1} controls><track kind="captions" /></video>
 
-<video id="user1" bind:this={video2} controls><track kind="captions" /></video>
+<video id="user2" bind:this={video2} controls><track kind="captions" /></video>
 
 <input type="file" on:change={handleFileChange} />
 
